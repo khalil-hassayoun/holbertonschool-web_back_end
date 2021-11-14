8d0ea031444c022e48146f17826b50edd5691661
khalil hassayoun <70943561+khalil-hassayoun@users.noreply.github.com>
2021-11-14 23:49:24 +0100

    " "

---
+++ 0x07-Session_authentication/models/user_session.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" UserSession module
+"""
+from models.base import Base
+
+
+class UserSession(Base):
+    """ UserSession class
+    """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """ Initialize a User instance
+        """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
