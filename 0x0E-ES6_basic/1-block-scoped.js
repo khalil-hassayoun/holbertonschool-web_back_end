19939d8ad69d439a0a9909922de3fb1821f5d910
khalil hassayoun <70943561+khalil-hassayoun@users.noreply.github.com>
2021-12-09 22:33:42 +0100

    " "

---
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    /* eslint-disable */
+    const task = true;
+    const task2 = false;
+    /* eslint-enable */
+  }
+
+  return [task, task2];
+}
